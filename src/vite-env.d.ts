75ccde1c284768d0bc840a8c2566b8b372136bed
desdiogo <71910114+desdiogo@users.noreply.github.com>
2023-12-30 23:03:54 +0000

    Build: (fccddd2) fix dir

--- src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-
-type SelectdTool = 'rectangle' | 'circle' | 'triangle' | 'line' | 'brush' | 'eraser'
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'development' ? '/' : '/paint',
-  }
-})
